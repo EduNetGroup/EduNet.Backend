a77457e770e9f366257497536b8af74890197a7a
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-02-18 13:22:44 +0500

    Create DbContext

---
+++ src/EduNet.Backend.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EduNet.Backend.Domain.Entities.Roles;
+using EduNet.Backend.Domain.Entities.Users;
+using EduNet.Backend.Domain.Entities.Courses;
+using EduNet.Backend.Domain.Entities.Branches;
+using EduNet.Backend.Domain.Entities.Payments;
+using EduNet.Backend.Domain.Entities.Students;
+using EduNet.Backend.Domain.Entities.Teachers;
+
+namespace EduNet.Data.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    DbSet<User> Users { get; set; }
+    DbSet<Role> Roles { get; set; }
+    DbSet<Course> Courses { get; set; }
+    DbSet<Lesson> Lessons { get; set; }
+    DbSet<Branch> Branches { get; set; }
+    DbSet<Teacher> Teachers { get; set; }
+    DbSet<Payment> Payments { get; set; }
+    DbSet<Student> Students { get; set; }
+    DbSet<UserRole> UserRoles { get; set; }
+    DbSet<UserCode> UserCodes { get; set; }
+    DbSet<Permission> Permissions { get; set; }
+    DbSet<Attendance> Attendances { get; set; }
+    DbSet<Enrollment> Enrollments { get; set; }
+    DbSet<TeacherCourse> TeachersCourse { get; set; }
+    DbSet<RolePermission> RolePermissions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
