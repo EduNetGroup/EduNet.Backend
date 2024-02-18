1a73260981f4764304cfa6b536f945663564bb55
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-02-18 17:07:46 +0500

    Create EduNetException

---
+++ src/EduNet.Backend.Service/Exceptions/EduNetException.cs
@@ -0,0 +1,11 @@
+﻿namespace EduNet.Backend.Service.Exceptions;
+
+partial class EduNetException : Exception
+{
+    public int statusCode;
+
+    public EduNetException(int Code, string Message) : base(Message)
+    {
+        statusCode = Code;
+    }
+}
