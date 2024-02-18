e7edba2fadc6915e32c1b5fe61eeef1df0e595e1
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-02-18 13:24:40 +0500

    Craete IRepository

---
+++ src/EduNet.Backend.Data/IRepositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using EduNet.Backend.Domain.Commons;
+
+namespace EduNet.Data.IRepositories;
+
+public interface IRepository<TEntity> where TEntity : Auditable
+{
+    public Task<bool> DeleteAsync(long id);
+    public Task<TEntity> UpdateAsync(TEntity entity);
+    public Task<TEntity> InsertAsync(TEntity entity);
+    public Task<bool> DeleteManyAsync(Expression<Func<TEntity, bool>> expression);
+    public Task<TEntity> SelectAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null);
+    public IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>> expression = null, string[] includes = null);
+}
