0c72d1e5d7f4db6b36fedbcd933b2ce64afe2bec
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-02-18 16:28:59 +0500

    Create Login DTOs

---
+++ src/EduNet.Backend.Service/DTOs/Logins/LoginForResultDto.cs
@@ -0,0 +1,12 @@
+﻿using EduNet.Backend.Service.DTOs.Users.Users;
+
+namespace EduNet.Backend.Service.DTOs.Logins;
+
+public class LoginForResultDto
+{
+    public long Id { get; set; }
+    public string Token { get; set; }
+    public DateTime AccessTokenExpireDate { get; set; }
+    public string RefreshToken { get; set; }
+    public UserForResultDto User { get; set; }
+}
