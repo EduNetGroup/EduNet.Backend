7c039c6cca923bb9ccacbc28b36f1c275239c9a3
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-02-18 13:09:27 +0500

    Create Auditable entity

---
+++ src/EduNet.Backend.Domain/Commons/Auditable.cs
@@ -0,0 +1,10 @@
+﻿namespace EduNet.Backend.Domain.Commons;
+
+public abstract class Auditable
+{
+    public long Id { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
