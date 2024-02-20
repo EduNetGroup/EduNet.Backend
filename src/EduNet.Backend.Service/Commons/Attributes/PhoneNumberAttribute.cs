c7a61e8210e17df3d98107bacf3ec7f77445fcb8
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-02-20 20:39:59 +0500

    Create EnvironmentHelper and PasswordHasherHelper

--- src/EduNet.Backend.Service/Commons/Attributes/PhoneNumberAttribute.cs
+++ src/EduNet.Backend.Service/Commons/Attributes/PhoneNumberAttribute.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+using System.ComponentModel.DataAnnotations;
 
-namespace EduNet.Backend.Service.Commons.Attributes
+namespace EduNet.Backend.Service.Commons.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class PhoneNumberAttribute : ValidationAttribute
 {
-    internal class PhoneNumberAttribute
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
+        string phoneNumber = (string)value;
+        if (String.IsNullOrWhiteSpace(phoneNumber))
+        {
+            return new ValidationResult("Please, Enter the correct phone number!");
+        }
+        else
+        {
+            Regex regex = new Regex("^(?:\\+998([- ])?(90|91|93|94|95|98|99|33|50|77|97|71|99|88)([- ])?(\\d{3})([- ])?(\\d{2})([- ])?(\\d{2}))");
+
+            return (regex.Match(phoneNumber).Success) ? ValidationResult.Success
+                : new ValidationResult("Please, Enter the correct phone number!");
+        }
     }
 }
