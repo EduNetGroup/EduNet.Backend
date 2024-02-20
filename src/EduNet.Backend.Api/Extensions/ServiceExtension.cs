4d84b722e8dc12ce47c677f9377667393acac5eb
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-02-21 01:07:12 +0500

    Implement IBranchService and BranchService is done also BranchesController

--- src/EduNet.Backend.Api/Extensions/ServiceExtension.cs
+++ src/EduNet.Backend.Api/Extensions/ServiceExtension.cs
@@ -1,6 +1,8 @@
 ﻿using EduNet.Backend.Service.Mappers;
 using EduNet.Backend.Data.Repositories;
 using EduNet.Backend.Data.IRepositories;
+using EduNet.Backend.Service.Services.Branches;
+using EduNet.Backend.Service.Interfaces.Branches;
 
 namespace EduNet.Backend.Api.Extensions;
 
@@ -15,5 +17,6 @@ public static class ServiceExtension
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
         // Services
+        services.AddScoped<IBranchService, BranchService>();
     }
 }
