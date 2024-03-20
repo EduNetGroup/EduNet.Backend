bfde7207e686deb1b98950b1599325a48c9456f9
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-03-20 16:53:15 +0500

    PermissionsController is completed

---
+++ src/EduNet.Backend.Api/Controllers/Roles/PermissionsController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EduNet.Backend.Api.Models;
+using EduNet.Backend.Service.Configurations;
+using EduNet.Backend.Api.Controllers.Commons;
+using EduNet.Backend.Service.Interfaces.Roles;
+using EduNet.Backend.Service.DTOs.Roles.Permissions;
+
+namespace EduNet.Backend.Api.Controllers.Roles;
+
+public class PermissionsController : BaseController
+{
+    private readonly IPermissionService _permissionService;
+
+    public PermissionsController(IPermissionService permissionService)
+    {
+        _permissionService = permissionService;
+    }
+
+    /// <summary>
+    /// To Add permission 
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] PermissionForCreationDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _permissionService.AddAsync(dto)
+        });
+
+    /// <summary>
+    /// To Get all permissions 
+    /// </summary>
+    /// <param name="params"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _permissionService.RetrieveAllAsync(@params)
+        });
+
+    /// <summary>
+    /// To Get permission by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute(Name = "id")] long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _permissionService.RetrieveByIdAsync(id)
+        });
+
+    /// <summary>
+    /// To Update permission by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync([FromRoute(Name = "id")] long id, [FromBody] PermissionForUpdateDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _permissionService.ModifyAsync(id, dto)
+        });
+
+    /// <summary>
+    /// To Delete permission by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute(Name = "id")] long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _permissionService.RemoveAsync(id)
+        });
+
+    /// <summary>
+    /// To Get permissions by searching
+    /// </summary>
+    /// <param name="search"></param>
+    /// <param name="params"></param>
+    /// <returns></returns>
+    [HttpGet("{search}")]
+    public async Task<IActionResult> GetAllBySearchAsync([FromQuery(Name = "search")] string search, [FromQuery] PaginationParams @params)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _permissionService.SearchAllAsync(search, @params)
+        });
+}
