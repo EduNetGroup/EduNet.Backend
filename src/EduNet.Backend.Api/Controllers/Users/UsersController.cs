5157db1184263d1ff355065d2eff7f856f9d2e75
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-04-27 18:32:17 +0500

    UsersController is completed

---
+++ src/EduNet.Backend.Api/Controllers/Users/UsersController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EduNet.Backend.Api.Models;
+using EduNet.Backend.Service.Configurations;
+using EduNet.Backend.Service.DTOs.Users.Users;
+using EduNet.Backend.Service.Interfaces.Users;
+using EduNet.Backend.Api.Controllers.Commons;
+
+namespace EduNet.Backend.Api.Controllers.Users;
+
+public class UsersController : BaseController
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    /// <summary>
+    /// To Create user
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] UserForCreationDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userService.AddAsync(dto)
+        });
+
+    /// <summary>
+    /// To Get all users
+    /// </summary>
+    /// <param name="params"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userService.RetrieveAllAsync(@params)
+        });
+
+    /// <summary>
+    /// To Get user by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync([FromRoute(Name = "id")] long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userService.RetrieveByIdAsync(id)
+        });
+
+    /// <summary>
+    /// To update user by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync([FromRoute(Name = "id")] long id, [FromBody] UserForUpdateDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userService.ModifyAsync(id, dto)
+        });
+
+    /// <summary>
+    /// To delete user by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute(Name = "id")] long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userService.RemoveAsync(id)
+        });
+
+    /// <summary>
+    /// To get all users by searching
+    /// </summary>
+    /// <param name="search"></param>
+    /// <param name="params"></param>
+    /// <returns></returns>
+    [HttpGet("search")]
+    public async Task<IActionResult> SearchAllAsync([FromQuery(Name = "search")] string search, [FromQuery] PaginationParams @params)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _userService.SearchAllAsync(search, @params)
+        });
+}
