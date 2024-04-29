0304162570691e0ee9c66884f46ad0875a2a8d2b
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-04-29 12:07:46 +0500

    AuthService is completed

--- src/EduNet.Backend.Service/Services/Auth/AuthService.cs
+++ src/EduNet.Backend.Service/Services/Auth/AuthService.cs
@@ -1,5 +1,68 @@
-﻿namespace EduNet.Backend.Service.Services.Auth;
+﻿using EduNet.Backend.Service.DTOs.Users.Users;
+using EduNet.Backend.Service.Interfaces.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
-public class AuthService
+namespace EduNet.Backend.Service.Services.Auth;
+
+public class AuthService : IAuthService
 {
+    private readonly ILogger<AuthService> _logger;
+    private readonly IConfiguration _configuration;
+    public AuthService(ILogger<AuthService> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+    public Task<(string refreshToken, DateTime tokenValidityTime)> GenerateRefreshTokenAsync()
+    {
+        var randomNumber = new byte[64];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+
+        if (!double.TryParse(_configuration["JWT:RefreshTokenValidityHours"], out double refreshTokenValidityHours))
+            refreshTokenValidityHours = 5;
+
+        var tokenExpiryTime = DateTime.UtcNow.AddHours(refreshTokenValidityHours);
+        return Task.FromResult((Convert.ToBase64String(randomNumber), tokenExpiryTime));
+    }
+
+    public Task<(string token, DateTime tokenExpiryTime)> GenerateTokenAsync(UserForResultDto user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenKey = Encoding.UTF8.GetBytes(_configuration["JWT:Key"]!);
+        var expireDate = DateTime.UtcNow.AddMinutes(double.Parse(_configuration["JWT:AccessTokenExpireMinutes"]!));
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+             new Claim("Id", user.Id.ToString()),
+             new Claim("Email", user.Email),
+             new Claim("Name", user.FirstName + " " + user.LastName),
+             new Claim("PhoneNumber", user.PhoneNumber),
+            }.Concat(user.Roles.Select(role => new Claim("Role", role)))),
+            Audience = _configuration["JWT:Audience"],
+            Issuer = _configuration["JWT:Issuer"],
+            IssuedAt = DateTime.UtcNow,
+            Expires = expireDate,
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return Task.FromResult((tokenHandler.WriteToken(token), expireDate));
+    }
+
+    public Task<UserForResultDto> GetUserByAccessTokenAsync(string accessToken)
+    {
+        throw new NotImplementedException();
+    }
 }
