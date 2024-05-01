3d7aa9c79aaecf812e3a749066ca479819a74089
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-05-01 22:54:36 +0500

    Update UserForCreationDto

--- src/EduNet.Backend.Service/DTOs/Users/Users/UserForCreationDto.cs
+++ src/EduNet.Backend.Service/DTOs/Users/Users/UserForCreationDto.cs
@@ -1,11 +1,26 @@
-﻿namespace EduNet.Backend.Service.DTOs.Users.Users;
+﻿using EduNet.Backend.Service.Commons.Attributes;
+using System.ComponentModel.DataAnnotations;
+using EmailAddressAttribute = EduNet.Backend.Service.Commons.Attributes.EmailAddressAttribute;
+
+namespace EduNet.Backend.Service.DTOs.Users.Users;
 
 public class UserForCreationDto
 {
+    [MinLength(1), MaxLength(64)]
     public string FirstName { get; set; }
+
+    [MinLength(1), MaxLength(64)]
     public string LastName { get; set; }
+
+    [Required]
     public long BranchId { get; set; }
+
+    [PhoneNumberAttribute]
     public string PhoneNumber { get; set; }
+
+    [EmailAddressAttribute]
     public string Email { get; set; }
+
+    [StrongPasswordAttribute]
     public string Password { get; set; }
 }
